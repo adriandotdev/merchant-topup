336d4c4b07e85b987d525cb72b7f2406bc7de8fa
Adrian <marcelonads@gmail.com>
2024-05-28 14:55:26 +0800

    docs: ADDED comments in services, and repositories files

--- repository/TopupRepository.js
+++ repository/TopupRepository.js
@@ -8,6 +8,17 @@ const mysql = require("../database/mysql");
 const Crypto = require("../utils/Crypto");
 
 module.exports = class TopupRepository {
+	/**
+	 * Verifies a top-up by identifier.
+	 *
+	 * This function retrieves user information based on the provided identifier (RFID tag or mobile number).
+	 * It searches for a user with the specified RFID tag or mobile number in the database and returns their details,
+	 * including user ID, name, address, email, mobile number, RFID tag, vehicle plate number, vehicle model, vehicle brand, and username.
+	 *
+	 * @function VerifyTopupByIdentifier
+	 * @param {string} identifier - The identifier (RFID tag or mobile number) associated with the user.
+	 * @returns {Promise<Object[]>} A promise that resolves to an array of user details if the identifier is found, or an empty array if not found.
+	 */
 	VerifyTopupByIdentifier(identifier) {
 		const QUERY = `
         SELECT 
@@ -44,6 +55,19 @@ module.exports = class TopupRepository {
 		});
 	}
 
+	/**
+	 * Initiates a top-up for a user account.
+	 *
+	 * This function triggers a top-up operation for the specified user account identified by RFID tag or mobile number.
+	 * It calls the stored procedure 'WEB_USER_MERCHANT_TOPUP' in the database to execute the top-up process.
+	 * The top-up amount and payment type are provided as parameters.
+	 *
+	 * @function Topup
+	 * @param {string} identifier - The identifier (RFID tag or mobile number) associated with the user account.
+	 * @param {number} amount - The amount to be topped up into the user account.
+	 * @param {string} paymentType - The payment type used for the top-up (e.g., 'CARD', 'MAYA', etc.).
+	 * @returns {Promise<Object[]>} A promise that resolves to an array containing the result of the top-up operation.
+	 */
 	Topup(identifier, amount, paymentType) {
 		const QUERY = `CALL WEB_USER_MERCHANT_TOPUP(?,?,?,?)`;
 
@@ -62,6 +86,19 @@ module.exports = class TopupRepository {
 		});
 	}
 
+	/**
+	 * Retrieves top-up records for a specific user ID.
+	 *
+	 * This function fetches top-up records from the database for the specified user ID within a certain time frame.
+	 * It selects records from the 'topup_logs' table where the user ID matches the provided userID parameter,
+	 * the current time is within 60 minutes from the top-up creation time, the type of transaction is 'TOPUP',
+	 * and the transaction has not been voided.
+	 *
+	 * @function GetTopupsByUserID
+	 * @param {number} userID - The ID of the user for whom top-up records are to be retrieved.
+	 * @param {Date} currentDateTime - The current date and time used for comparison to determine voidable transactions.
+	 * @returns {Promise<Object[]>} A promise that resolves to an array containing the top-up records for the specified user.
+	 */
 	GetTopupsByUserID(userID, currentDateTime) {
 		const QUERY = `
 			SELECT 
@@ -87,6 +124,16 @@ module.exports = class TopupRepository {
 		});
 	}
 
+	/**
+	 * Voids a top-up transaction based on the provided reference number.
+	 *
+	 * This function voids a top-up transaction by calling a stored procedure in the database
+	 * with the given reference number as a parameter.
+	 *
+	 * @function VoidTopupByReferenceNumber
+	 * @param {string} referenceNumber - The reference number of the top-up transaction to be voided.
+	 * @returns {Promise<Object[]>} A promise that resolves to the result of the voiding operation.
+	 */
 	VoidTopupByReferenceNumber(referenceNumber) {
 		const QUERY = `CALL WEB_USER_MERCHANT_VOID_TOPUP(?)`;
 
--- services/TopupService.js
+++ services/TopupService.js
@@ -16,6 +16,17 @@ module.exports = class TopupService {
 		this.#repository = new TopupRepository();
 	}
 
+	/**
+	 * Verifies a top-up by identifier.
+	 *
+	 * This function verifies a top-up using the provided identifier. It retrieves the user associated with the top-up, decrypts sensitive user information,
+	 * and returns the decrypted user data.
+	 *
+	 * @function VerifyTopupByIdentifier
+	 * @param {string} identifier - The identifier associated with the top-up.
+	 * @returns {Promise<Object>} A promise that resolves to the decrypted user data associated with the top-up.
+	 * @throws {HttpBadRequest} If the account associated with the identifier is not found.
+	 */
 	async VerifyTopupByIdentifier(identifier) {
 		const result = await this.#repository.VerifyTopupByIdentifier(identifier);
 
@@ -33,6 +44,20 @@ module.exports = class TopupService {
 		return decryptedUser;
 	}
 
+	/**
+	 * Initiates a top-up transaction.
+	 *
+	 * This function initiates a top-up transaction with the provided identifier, amount, and payment type.
+	 * It interacts with the repository to perform the top-up operation and returns the status of the transaction
+	 * along with the new balance if the transaction is successful.
+	 *
+	 * @function Topup
+	 * @param {string} identifier - The identifier associated with the user account.
+	 * @param {number} amount - The amount to be topped up.
+	 * @param {string} paymentType - The type of payment for the top-up (e.g., "CARD", "MAYA").
+	 * @returns {Promise<Object|string>} A promise that resolves to an object containing the status of the top-up transaction and the new balance
+	 * if the transaction is successful. If the transaction fails, it returns the status message.
+	 */
 	async Topup(identifier, amount, paymentType) {
 		const result = await this.#repository.Topup(
 			identifier,
@@ -50,6 +75,18 @@ module.exports = class TopupService {
 		return { STATUS, new_balance };
 	}
 
+	/**
+	 * Retrieves top-up transactions by user ID and current date and time.
+	 *
+	 * This function retrieves top-up transactions associated with the specified user ID
+	 * up to the provided current date and time. It interacts with the repository to
+	 * fetch the relevant top-up data.
+	 *
+	 * @function GetTopupsByUserID
+	 * @param {string} userID - The ID of the user whose top-up transactions are to be retrieved.
+	 * @param {Date} currentDateTime - The current date and time used as a reference for filtering top-up transactions.
+	 * @returns {Promise<Array>} A promise that resolves to an array containing the top-up transactions matching the specified user ID and current date and time.
+	 */
 	async GetTopupsByUserID(userID, currentDateTime) {
 		const result = await this.#repository.GetTopupsByUserID(
 			userID,
@@ -59,6 +96,17 @@ module.exports = class TopupService {
 		return result;
 	}
 
+	/**
+	 * Voids a top-up transaction by reference number.
+	 *
+	 * This function voids the top-up transaction associated with the specified reference number.
+	 * It interacts with the repository to perform the void operation and retrieves the status,
+	 * reference number, and current balance after voiding.
+	 *
+	 * @function VoidTopupByReferenceNumber
+	 * @param {string} referenceNumber - The reference number of the top-up transaction to be voided.
+	 * @returns {Promise<Object|string>} A promise that resolves to an object containing the voided top-up transaction details (including status, reference number, and current balance) if successful, or a string representing the error status if unsuccessful.
+	 */
 	async VoidTopupByReferenceNumber(referenceNumber) {
 		const result = await this.#repository.VoidTopupByReferenceNumber(
 			referenceNumber
